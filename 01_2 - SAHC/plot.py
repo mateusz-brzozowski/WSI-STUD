024050060c3de48fb95320b4718f66f1f5827b88
Mateusz Brzozowski <01158596@pw.edu.pl>
2021-10-26 18:44:37 +0200

    changnge iterations for stop  function

--- 01_2 - SAHC/plot.py
+++ 01_2 - SAHC/plot.py
@@ -3,7 +3,8 @@ import numpy as np
 
 
 def draw_plot(function, bounds, plot_step):
-    x_arr = y_arr = np.arange(-bounds, bounds, plot_step)
+    x_arr = np.arange(-bounds, bounds, plot_step)
+    y_arr = np.arange(-bounds, bounds, plot_step)
     X, Y = np.meshgrid(x_arr, y_arr)
     Z = np.empty(X.shape)
 
@@ -15,18 +16,24 @@ def draw_plot(function, bounds, plot_step):
 
 
 def draw_arrows(points, scale):
-    for i in range(len(points)-1):
-        delta = points[i+1] - points[i]
+    for i in range(1, len(points)-1):
+        dx = points[i+1] - points[i]
         plt.arrow(
             points[i][0], points[i][1],
-            delta[0], delta[1],
+            dx[0], dx[1],
             head_width=scale * 0.05, head_length=scale * 0.01,
             fc='k', ec='k')
 
 
 def create_plot(fun, points):
-    MAX_POINT = np.amax(points)
-    PLOT_STEP = MAX_POINT / 100
+    point_array = []
+    for point in points:
+        x_array = []
+        for x in point:
+            x_array.append(abs(x))
+        point_array.append(max(x_array))
+    MAX_POINT = max(point_array)
+    PLOT_STEP = MAX_POINT / 200
 
     draw_plot(fun, MAX_POINT, PLOT_STEP)
     draw_arrows(points, MAX_POINT)
--- 01_2 - SAHC/steepest_ascent.py
+++ 01_2 - SAHC/steepest_ascent.py
@@ -6,24 +6,34 @@ import numpy as np
 def booth(x):
     return (x[0] + 2*x[1] - 7)**2 + (2*x[0] + x[1] - 5)**2
 
-def steepest_ascent(function, x, beta, n):
+def steepest_ascent(function, x, beta):
     dx = x.copy()
-    points = []
-    for _ in range(n):
+    points = [dx]
+    while not stop(function, points):
         grad_fct = grad(function)
-        dx -= beta*grad_fct(dx)
+        dx += beta*grad_fct(dx)
         points.append(dx.copy())
     return points
 
+def stop(function, points):
+    if len(points) > 1000:
+        return True
+    dx = 0.001
+    if (function(points[-1]) >= function(points[-1] + [0, dx]) and
+        function(points[-1]) >= function(points[-1] + [0, -dx]) and
+        function(points[-1]) >= function(points[-1] + [dx, 0]) and
+        function(points[-1]) >= function(points[-1] + [dx, 0])):
+        return True
+
 def main():
-    UPPER_BOUND = 200
+    UPPER_BOUND = 100
     DIMENSIONALITY = 2
     x = np.random.uniform(-UPPER_BOUND, UPPER_BOUND, size=DIMENSIONALITY)
     print("1-st point: ", x)
-    # points = steepest_ascent(f1, x, 0.00000001, 40)
-    # points = steepest_ascent(f2, x, 0.5, 400)
-    # points = steepest_ascent(f3, x, 0.00005, 40)
-    points = steepest_ascent(booth, x, 0.05, 40)
+    # points = steepest_ascent(f1, x, 0.00000001)
+    # points = steepest_ascent(f2, x, 0.5)
+    # points = steepest_ascent(f3, x, 0.00005)
+    points = steepest_ascent(booth, x, 0.05)
     create_plot(booth, points)
 
 
