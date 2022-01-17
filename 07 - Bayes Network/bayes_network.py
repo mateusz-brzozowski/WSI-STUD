7804225613b2ae8eaa2dd34ba64cca19d58dd218
Mateusz Brzozowski <01158596@pw.edu.pl>
2022-01-17 16:15:20 +0100

    remove class

--- 07 - Bayes Network/bayes_network.py
+++ 07 - Bayes Network/bayes_network.py
@@ -1,30 +1,12 @@
 import json
 import csv
 from random import uniform
-from collections import OrderedDict
 
 class Node:
     def __init__(self, name, parents = None, probabilities = None):
         self.name = name
         self.parents = parents
         self.probabilities = probabilities
-
-class Sample:
-    def __init__(self, nodes):
-        self.values = OrderedDict()
-        for node in nodes:
-            parent_values = ""
-            for name in node.parents:
-                parent_values += 'T' if self.values[name] else 'F'
-            self.values[node.name] = (
-                uniform(0, 1) < node.probabilities[parent_values]
-            )
-
-    def to_string(self):
-        result = ''
-        for val in self.values.values():
-            result += f"{val},"
-        return result[:-1]
 
 def load_from_json(path_file):
     with open(path_file, 'r') as file_handle:
@@ -37,7 +19,17 @@ def load_from_json(path_file):
 def generate_and_save(path_file, nodes, number_of_examples):
     with open(path_file, 'w') as file_handle:
         for _ in range(number_of_examples):
-            file_handle.write(Sample(nodes).to_string() + '\n')
+            values = {}
+            result = ''
+            for node in nodes:
+                parent_values = ""
+                for name in node.parents:
+                    parent_values += 'T' if values[name] else 'F'
+                values[node.name] = (
+                    uniform(0, 1) < node.probabilities[parent_values]
+                )
+                result += f"{values[node.name]},"
+            file_handle.write(result[:-1] + '\n')
 
 def main():
     nodes = load_from_json("07 - Bayes Network/input.json")
